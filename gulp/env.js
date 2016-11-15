a2e80b920648bd59bb331e9dc3768350469953f7
Thierry Charbonnel <thierry@creationflux.com>
2016-11-15 07:46:03 +0100

    get version

--- gulp/env.js
+++ gulp/env.js
@@ -2,12 +2,22 @@ import args from './support/args';
 import gulp from 'gulp';
 import { execSync } from 'child_process';
 
+// Version getter.
+const getVersion = () => {
+   var version;
+   try {
+     version = process.env.SOURCE_VERSION || process.env.SHA || execSync('git rev-parse HEAD').toString().trim();;
+   } catch (e) {
+     version = 'unknown';
+   }
+   return version;
+};
+
+const VERSION = getVersion();
+
 gulp.task('env', () => {
   process.env.NODE_ENV = args.production ? 'production' : 'development';
   // The app is not a library, so it doesn't make sense to use semver.
   // Este uses appVersion for crash reporting to match bad builds easily.
-  const gitIsAvailable = !process.env.SOURCE_VERSION; // Heroku detection.
-  if (gitIsAvailable) {
-    process.env.appVersion = execSync('git rev-parse HEAD').toString().trim();
-  }
+  process.env.appVersion = VERSION;
 });
